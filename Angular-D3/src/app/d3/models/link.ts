a496b58873962c1f62ef95ae817fa77511f8a56f
erikedmonds <eredmonds@millikin.edu>
2018-09-21 16:40:35 -0500

    Added Nodes

---
+++ Angular-D3/src/app/d3/models/link.ts
@@ -0,0 +1,13 @@
+import { Node } from './';
+
+export clas Link implements d3.SimulationLinkDatum<Node> {
+	index?: number;
+
+	source: Node | string | number;
+	target: Node | string | number;
+
+	constructor(source, target) {
+		this.source = source;
+		this.target = target;
+	}
+}
