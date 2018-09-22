d92d463201fbb21f82523a59de596daf8ec215f8
Erik Edmonds <eedmo3@illinois.edu>
2018-09-22 10:26:07 -0500

    Updated app

---
+++ Angular-D3-Js/src/app/d3/models/node.ts
@@ -0,0 +1,15 @@
+export class Node extends d3.SimulationNodeDatum {
+  index?: number;
+  x?: number;
+  y?: number;
+  vx?: number;
+  vy?: number;
+  fx?: number | null;
+  fy?: number | null;
+
+  id: string;
+  contructor(id) {
+    this.id = id;
+  }
+
+}
