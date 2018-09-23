818dce4796cf40bef6e9dfd6c79e7eff9262a46c
Erik Edmonds <eedmo3@illinois.edu>
2018-09-22 20:15:51 -0500

    Updated app

--- Angular-D3-Js/src/app/d3/models/node.ts
+++ Angular-D3-Js/src/app/d3/models/node.ts
@@ -1,4 +1,7 @@
-export class Node extends d3.SimulationNodeDatum {
+import APP_CONFIG from '../../app.config';
+
+export class Node implements d3.SimulationNodeDatum {
+  // optional - defining optional implementation properties - required for relevant typing assistance
   index?: number;
   x?: number;
   y?: number;
@@ -8,9 +11,26 @@ export class Node extends d3.SimulationNodeDatum {
   fy?: number | null;
 
   id: string;
-  
-  contructor(id) {
+  linkCount: number = 0;
+
+  constructor(id) {
     this.id = id;
   }
 
+  normal = () => {
+    return Math.sqrt(this.linkCount / APP_CONFIG.N);
+  }
+
+  get r() {
+    return 50 * this.normal() + 10;
+  }
+
+  get fontSize() {
+    return (30 * this.normal() + 10) + 'px';
+  }
+
+  get color() {
+    let index = Math.floor(APP_CONFIG.SPECTRUM.length * this.normal());
+    return APP_CONFIG.SPECTRUM[index];
+  }
 }
